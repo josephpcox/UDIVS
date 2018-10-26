4b3ff69980334fb4aa549e13aadc41e4f667f9d4
Joseph Cox <josephcox@Josephs-MacBook-Air.local>
2018-10-26 03:40:50 -0400

    began a python script that creates pandas data frames after day one

---
+++ src_dataAnalysis/Analysis.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 26 03:24:16 2018
+
+@author: josephcox
+"""
+
+# Load the Pandas libraries with alias 'pd' 
+import pandas as pd 
+# Read data from file 'filename.csv' 
+# (in the same directory that your python process is based)
+# Control delimiters, rows, column names with read_csv (see later) 
+joe_app_usage = pd.read_csv("../userdevice_data/Joe_Data/App_Usage/Day1.csv")
+joe_timeslots = pd.read_csv("../userdevice_data/Joe_Data/Smarter_time/timeslots_day1.csv")
+joe_geoloc =  pd.read_csv("../userdevice_data/Joe_Data/Smarter_time/geoloc_day1.csv")
