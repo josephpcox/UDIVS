6a523e518104d8de2b8909cef0ff99aa4469bc6e
Joseph Cox <josephcox@USF-Gold-Wifi-226-4-57.laptops.usf.edu>
2018-11-07 22:21:48 -0500

    progress: started to make skelton for questions and answers: also sorted the python data frame and more...

--- src_dataAnalysis/Tom_src/myAnalysis.py
+++ src_dataAnalysis/Tom_src/myAnalysis.py
@@ -8,7 +8,9 @@ Created on Sat Oct 27 23:11:33 2018
 from datetime import timedelta
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import random
+import numpy as np
+from operator import itemgetter
 #import numpy as np
 #from sklearn.feature_selection import VarianceThreshold
 
@@ -17,7 +19,7 @@ import matplotlib.pyplot as plt
 #gets day and location
 def getLocation(DataFrame):
     #filters the Day and Place column only
-    filtered = DataFrame[['Day', 'Place']].copy()
+    filtered = DataFrame[['Day', 'Place','Time']].copy()
     
     #remove rows with Nan in any column
     df = filtered.dropna()
@@ -31,15 +33,15 @@ def getActivity(DataFrame):
     #remove rows with Nam in any column
     df = filtered.dropna()
     
-#    final = df[(df.Activity != 'walk')]
-#    final = final[(final.Activity != 'lesson')]
-#    final = final[(final.Activity != 'home time')]
-#    final = final[(final.Activity != 'vehicle')]
-#    final = final[(final.Activity != 'groceries')]
-#    final = final[(final.Activity != 'sleep')]
-#    final = final[(final.Activity != 'drinks')]
-#    final = final[(final.Activity != 'religion')]
-#    final = final[(final.Activity != 'exhibition')]
+    final = df[(df.Activity != 'walk')]
+    final = final[(final.Activity != 'lesson')]
+    final = final[(final.Activity != 'home time')]
+    final = final[(final.Activity != 'vehicle')]
+    final = final[(final.Activity != 'groceries')]
+    final = final[(final.Activity != 'sleep')]
+    final = final[(final.Activity != 'drinks')]
+    final = final[(final.Activity != 'religion')]
+    final = final[(final.Activity != 'exhibition')]
     return df
 
 def getWeek(DataFrame):
@@ -54,6 +56,72 @@ def getData(DataFrame, Amount):
     
     return lastday[lastindex]
 
+def getRecentApp():
+    for x in tomDay_df['Activity'][::-1]:
+        if "phone:" not in x:
+            continue
+        ans = x
+        break
+    return ans
+
+def getRecentLocation():
+    pass
+
+def getOptions(n):
+    
+    options = []
+    
+    #question options for "which app did you use most recently
+    if n == 0:
+        """'Which app did you use most recently?'"""
+        ans = getRecentApp()
+        options.append(ans)
+        count = 1
+        
+        #this loop gives an array of answers called options for the user to choose from
+        for x in tomDay_df['Activity']:
+            flag = 0
+            if "phone:" in x:
+                for y in options:
+                    if x == y:
+                        flag = 1
+                if flag == 0:
+                    options.append(x)
+                    count = count +1
+                if count == 4:
+                    break
+        random.shuffle(options,random.random)
+        return ans,options
+    
+    elif n == 1:
+        """'What place were you at most recently?'"""
+        ans = getRecentLocation()
+        options.append(ans)
+        count = 1
+        
+        #this loop gives an array of answers called options for the user to choose from
+        for x in location['Activity']:
+            flag = 0
+            if "phone:" in x:
+                for y in options:
+                    if x == y:
+                        flag = 1
+                if flag == 0:
+                    options.append(x)
+                    count = count +1
+                if count == 4:
+                    break
+        random.shuffle(options,random.random)
+        return ans,options
+        pass
+    elif n == 2:
+        """'which place were you at around:'"""
+        pass
+    elif n == 3:
+        """Which of these places did you go to yesterday?'"""
+        pass
+    
+
 
 data = pd.read_csv('../../userdevice_data/Tom_Data/Smarter_time/SmarterTimeTimeslots.csv')
 
@@ -61,21 +129,42 @@ location = getLocation(data)
 
 Activity = getActivity(data)
 
-data1 = getData(Activity, 1)    
 
-#lastday = Activity.iloc[:,1]
-#lastindex = len(Activity.index)
 
 
 #------------------------------#
 #filter to one day
-tomDay = data[data.Day == 20181023]
-
+tomDay_df = data[data.Day == 20181023]
 
 #get location and activity
-tomDay = tomDay['Activity'].value_counts().to_dict()
+tomDay_dict = tomDay_df['Activity'].value_counts().to_dict()
 
-from operator import itemgetter
-sortedList = sorted(tomDay.items(), key=itemgetter(1), reverse = True)
-#plt.bar(sortedList.keys(), sortedList.values(), width=.8, align='edge', color='g')
+#tom's day sorted from greatest to least
+sortedTomDay = sorted(tomDay_dict.items(), key=itemgetter(1), reverse = True)
 
+#-------------------------------------------------------------------------------------------------------------------------#
+questions=['Which app did you use most recently?','What place were you at most recently?','which place were you at around:','Which of these places did you go to yesterday?']
+randomNums=random.sample(range(0,3),3)
+score = 0
+count = 1
+for n in randomNums:
+    if n != 0:
+        continue
+    print(questions[n])
+    ans,options = getOptions(n)
+    print(ans)
+    for o in options:
+        print(count,". ",o)
+        count= count+1
+    userAns=int(input("input answer here: ")) # Utilize Switch CasegetOptions(n)
+    if n == 0:
+        if ans == options[userAns-1]:
+            score = score+1
+    if n == 1:
+        pass
+    if n == 2:
+        pass
+    if n == 3:
+        pass
+
+print(score)
