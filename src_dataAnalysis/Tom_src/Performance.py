3b8a04ac4a0aa91c4fed621212cc48984c749627
Tom Nguyen <nguyen41@mail.usf.edu>
2018-11-20 03:40:18 -0500

    waited for joe to push

--- src_dataAnalysis/Tom_src/Performance.py
+++ src_dataAnalysis/Tom_src/Performance.py
@@ -51,59 +51,59 @@ plt.title('Score Distribution with d\' = ' + str(round(int(d_prime), 2)))
 plt.xlim([0.0, 1.0])
 plt.show()
 
-thresholds = np.linspace(0.0, 1.0,)
-far = []
-tpr = []
-frr = []
-
-for t in thresholds:
-    tp = 0
-    tn = 0
-    fp = 0
-    fn = 0
-    
-    for g_s in genuine_scores:
-        if g_s <= t:
-            tp += 1
-        else: 
-            fn += 1
-    
-    for i_s in imposter_scores:
-        if i_s <= t:
-            fp += 1
-        else:
-            tn += 1
-            
-    far.append(fp / (fp + tn))
-    tpr.append(tp / (tp + fn))
-    frr.append(fn / (fn + tp))
-
-min_distance = abs(frr[1] - far[1])       
-
-for (i,j) in zip(frr, far):
-    distance = abs(i - j)
-    
-    if distance < min_distance:
-        min_distance = distance
-        x = i
-        y = j
-
-EER = (x + y)/2
-        
-plt.figure()
-plt.plot(far,frr,lw = 1,color = 'blue')
-plt.xlabel('False Reject Rate')
-plt.ylabel('False Accept Rate')
-plt.title('Detection Error Tradeoff with EER = ' + str(round(EER, 3)))
-plt.show()
-
-plt.figure()
-plt.plot(far, tpr, color='darkorange', lw=1)
-plt.plot([0, 1], [0, 1], color='navy', lw=1, linestyle='--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver Operating Characteristic')
-plt.legend(loc="lower right")
-plt.show()
+#thresholds = np.linspace(0.0, 1.0,)
+#far = []
+#tpr = []
+#frr = []
+#
+#for t in thresholds:
+#    tp = 0
+#    tn = 0
+#    fp = 0
+#    fn = 0
+#    
+#    for g_s in genuine_scores:
+#        if g_s <= t:
+#            tp += 1
+#        else: 
+#            fn += 1
+#    
+#    for i_s in imposter_scores:
+#        if i_s <= t:
+#            fp += 1
+#        else:
+#            tn += 1
+#            
+#    far.append(fp / (fp + tn))
+#    tpr.append(tp / (tp + fn))
+#    frr.append(fn / (fn + tp))
+#
+#min_distance = abs(frr[1] - far[1])       
+#
+#for (i,j) in zip(frr, far):
+#    distance = abs(i - j)
+#    
+#    if distance < min_distance:
+#        min_distance = distance
+#        x = i
+#        y = j
+#
+#EER = (x + y)/2
+#        
+#plt.figure()
+#plt.plot(far,frr,lw = 1,color = 'blue')
+#plt.xlabel('False Reject Rate')
+#plt.ylabel('False Accept Rate')
+#plt.title('Detection Error Tradeoff with EER = ' + str(round(EER, 3)))
+#plt.show()
+#
+#plt.figure()
+#plt.plot(far, tpr, color='darkorange', lw=1)
+#plt.plot([0, 1], [0, 1], color='navy', lw=1, linestyle='--')
+#plt.xlim([0.0, 1.0])
+#plt.ylim([0.0, 1.05])
+#plt.xlabel('False Positive Rate')
+#plt.ylabel('True Positive Rate')
+#plt.title('Receiver Operating Characteristic')
+#plt.legend(loc="lower right")
+#plt.show()
